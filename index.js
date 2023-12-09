d0a81f7e7549bfc474bacf3cddc78ef4dc365579
Mark Vagil <83776097+markvagil@users.noreply.github.com>
2023-12-09 00:39:16 -0700

    adding vercel

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://ticket-tracking-system-delta.vercel.app/",
+    ],
     credentials: false,
   })
 );
