75bb4415336ef3fe1edb74403b4d535307b81d45
Mark Vagil <83776097+markvagil@users.noreply.github.com>
2023-12-09 00:48:10 -0700

    trying to fix

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.options("*", cors());
+
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://ticket-tracking-system-delta.vercel.app/",
+      "https://ticket-tracking-system-delta.vercel.app",
     ],
     credentials: false,
   })
